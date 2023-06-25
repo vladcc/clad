8afba7d7ba0917b226c822d96886300392204c78
Vladimir Dinev <vld.dinev@gmail.com>
2023-06-25 18:40:55 +0200

    clad 1.1.1

--- clad/src/main.c
+++ clad/src/main.c
@@ -15,7 +15,7 @@
 
 // <prog-info>
 static const char prog_name[] = "clad";
-static const char prog_version[] = "1.1";
+static const char prog_version[] = "1.1.1";
 
 static void print_usage_quit(void)
 {
